59850aded13c0020826622cebc2435f4cfe783c5
ekonenko <konenko.evg@yandex.ru>
2022-02-04 17:45:47 +0300

    HW_6_1

--- FirstApp/FriendsList/ViewController/FriendsListTableViewController.swift
+++ FirstApp/FriendsList/ViewController/FriendsListTableViewController.swift
@@ -11,6 +11,7 @@ class FriendsListTableViewController: UITableViewController {
     
     var contactList = [FriendsListCellModel]()
     
+
     func fillContactList(){
         let friend1 = FriendsListCellModel(name: "Vladimir", surname: "Putin", avatar: UIImage(named: "putin_avatar")!, photos:[ UIImage(named: "rus_1")!, UIImage(named: "rus_2")!, UIImage(named: "rus_3")!])
         let friend2 = FriendsListCellModel(name: "Angela", surname: "Merkel", avatar: UIImage(named: "merkel_avatar")!, photos:[ UIImage(named: "ger_1")!, UIImage(named: "ger_2")!, UIImage(named: "ger_3")!])
@@ -82,8 +83,22 @@ class FriendsListTableViewController: UITableViewController {
         performSegue(withIdentifier: friendsToPhotosSegue, sender: arrayByLetter(sourceArray: contactList, letter: arrayLetter(sourceArray: contactList)[indexPath.section])[indexPath.row])
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return arrayLetter(sourceArray: contactList)[section].uppercased()
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return arrayLetter(sourceArray: contactList)[section].uppercased()
+//    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView()
+        header.backgroundColor = UIColor.lightGray.withAlphaComponent(0.3) // прозрачность только хедера
+        //header.alpha = 0.3
+        
+        let leter: UILabel = UILabel(frame: CGRect(x: 30, y: 5, width: 20, height: 20))
+        leter.textColor = UIColor.black.withAlphaComponent(0.5)  // прозрачность только надписи
+        leter.text = arrayLetter(sourceArray: contactList)[section].uppercased()
+        leter.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.light)
+        header.addSubview(leter)
+        
+        return header
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
